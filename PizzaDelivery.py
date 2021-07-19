a03a86da178bf1d7b494528eebb8f893217d98b7
Anand <iamtheanandsingh@gmail.com>
2021-07-20 03:33:30 +0530

    20072021

---
+++ PizzaDelivery.py
@@ -0,0 +1,9 @@
+from tkinter.tix import InputOnly
+
+
+print("\nWelcome To Pizzaz!!")
+
+size = input("Enter Size S/M/L : ")
+
+pep = input("Add Pepperoni? Y/N : ")
+cheese = input("Add Cheese? Y/N : ")
