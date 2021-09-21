f351c952f268d5d18ac604d0b61717016e8b4e58
Anand <iamtheanandsingh@gmail.com>
2021-09-21 21:57:41 +0530

    21092021

---
+++ MulMatrix.py
@@ -0,0 +1,15 @@
+x = int(input("Enter Rows : "))
+y = int(input("Enter Columns : "))
+
+arr1 = []
+arr2 = []
+
+for i in range(0,x):
+    for j in range(0,y):
+        z = input()
+        arr1.append(z)
+
+for i in range(0,x):
+    for j in range(0,y):
+        p = input()
+        arr2.append(p)
