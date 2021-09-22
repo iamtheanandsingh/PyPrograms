518c46632858acc4a60f07664e6ada69a7b3fe5b
Anand <iamtheanandsingh@gmail.com>
2021-09-22 20:11:15 +0530

    22092021

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+x = int(input("Enter Roof : "))
+
+for i in range(0, x+1):
+    if i%3==0 and i%5 == 0:
+        print("FizzBuzz")
+    elif i&3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    else:
+        print(i)
